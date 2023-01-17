ebad70d6c77d731fa65ba1e7573e204ed86254dc
sonu7524 <sonudaryani135@gmail.com>
2023-01-17 14:12:22 +0530

    sol-3

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -7,26 +7,25 @@ public class Pizza {
     private String bill;
 
     private boolean cheese;
+    private boolean paperbag;
     private boolean extratopping;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        System.out.print("Base Price Of The Pizza: ");
-    }
-
-    public int getPrice(){
-        if(isVeg){
+        if(isVeg == true){
             this.price = 300;
         }
         else {
             this.price = 400;
         }
-        return price;
+    }
+
+    public int getPrice(){
+        return this.price;
     }
     public void addExtraCheese(){
         if(cheese == false) {
-            System.out.println("Extra Cheese Added: 80");
-            price += 80;
+            this.price += 80;
             this.cheese = true;
         }
     }
@@ -34,24 +33,30 @@ public class Pizza {
     public void addExtraToppings(){
         if(extratopping==false){
             if(isVeg == true){
-                price += 70;
-                System.out.println("Extra Toppings Added: 70");
+                this.price += 70;
             }
             else{
-                System.out.println("Extra Toppings Added: 120");
-                price += 120;
+                this.price += 120;
             }
             this.extratopping = true;
         }
     }
 
     public void addTakeaway(){
-            System.out.println("Paperbag Added: 20");
-            price += 20;
+        this.paperbag = true;
+        this.price += 20;
     }
 
     public String getBill(){
-        bill = "Total bill :"+price;
+        if(isVeg) System.out.println("Base Price Of The Pizza: 300");
+        else System.out.println("Base Price Of The Pizza: 400");
+        if(cheese==true) System.out.println("Extra Cheese Added: 80");
+        if(extratopping== true){
+            if(isVeg) System.out.println("Extra Toppings Added: 70");
+            else System.out.println("Extra Toppings Added: 120");
+        }
+        if(paperbag == true) System.out.println("Paperbag Added: 20");
+        this.bill = "Total bill :"+this.price;
         return this.bill;
     }
 }
